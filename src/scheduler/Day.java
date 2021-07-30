38fdc572f77414bbfe5c8530d0312aae779c3360
Cdogsnappy <craschka@wisc.edu>
2021-07-30 11:26:06 -0500

    Day and Week

---
+++ src/scheduler/Day.java
@@ -0,0 +1,11 @@
+package scheduler;
+
+public class Day {
+	private int[] array;
+	
+	public Day() {
+		array = new int[]{8,9,10,11,12,1,2,3,4,5};
+	}
+	
+
+}
---
+++ src/scheduler/Week.java
@@ -0,0 +1,13 @@
+package scheduler;
+import java.io.*;
+import java.util.*;
+
+public class Week{
+	private Day[] week;
+	
+	public Week() {
+		week = new Day[]{new Day(), new Day(), new Day(), new Day(), new Day()};
+	}
+	
+
+}
